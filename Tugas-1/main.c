3da6d22f1582850f3a3a9df326e6ce22e40736b7
Ghani Rafif <ghani.rafif.2112@gmail.com>
2020-09-22 18:30:19 +0700

    Init

---
+++ Tugas-1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  /* Init var */
+  char name[100] = "Ghani Rafif Irawan";
+  char birth[100] = "Jayapura, 21 Desember 2000";
+  int age = 19;
+
+  printf("Nama: %s\n", name);
+  printf("Umur: %d\n", age);
+  printf("Ttl : %s\n", birth);
+}
